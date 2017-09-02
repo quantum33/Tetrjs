7ab4f2157f31c6adf1dcdaf9b270a98bcf3a45e9
Thierry Castel <thierry.castel33@gmail.com>
2017-09-02 19:17:37 +0200

    clean for src

--- app/constants.js
+++
@@ -1,4 +0,0 @@
-const settings1 = "settings1";
-const settings2 = [1, 10, 5, 9, 14];
-
-export {settings1, settings2};
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    entry: ["./app/index.js"],
-    output: {
-        path: "C:\\Dev\\Javascript\\Tetris\\build",
-        filename: "bundle.js"
-    }
-};
